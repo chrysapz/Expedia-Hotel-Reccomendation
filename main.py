a060cd6fc14401c3f41e6d7546c84260c70d72ec
Chrysoula Pozrikidou <xrysa00@hotmail.com>
2023-05-08 23:35:03 +0200

    commited models are dummies -> ignore

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import argparse
 import pickle
 import os
 import gc
+import lightgbm
+from lightgbm import early_stopping, log_evaluation
 
 import pandas as pd
 import numpy as np
@@ -24,6 +26,10 @@ def load_data(file_path):
     gc.collect()
     print("Loading data from file {} ...".format(file_path))
     df = pd.read_csv(file_path)
+
+    #Remove when training as whole
+    #df = df.sample(n=1000000, random_state=42)
+
     print("Finished loading data....")
     return df
 
@@ -107,7 +113,8 @@ def plot_distr(df):
 def to_boolean(x):
  return True if x==1 else False if x==0 else None
 
-def preprocess(df):
+def preprocess(df, train=True):
+    gc.collect()
 
     #fill with -1 to differentiate between missing and actual ~1.6 million missing vs
     df['orig_destination_distance'] = df['orig_destination_distance'].fillna(-1)
@@ -123,7 +130,7 @@ def preprocess(df):
     df_processed = df.copy()
     df_processed['date_time'] = pd.to_datetime(df_processed['date_time'])
 
-    df_processed['year'] = df_processed['date_time'].dt.year
+    #df_processed['year'] = df_processed['date_time'].dt.year
     df_processed['month'] = df_processed['date_time'].dt.month
 
     df_processed = df_processed.drop('date_time', axis=1)    
@@ -153,15 +160,18 @@ def preprocess(df):
     '''
     comp_rate_cols = [f'comp{i}_rate' for i in range(1, 9)]
     df['comp_rate_sum'] = df[comp_rate_cols].fillna(0).sum(axis=1)
+    df = df.drop(columns = [f'comp{i}_rate' for i in range(1, 9)], axis=1)
 
     comp_inv_cols = [f'comp{i}_inv' for i in range(1, 9)]
     df['comp_inv_sum'] = df[comp_inv_cols].fillna(0).sum(axis=1)
+    df = df.drop(columns = [f'comp{i}_inv' for i in range(1, 9)], axis=1)
 
     comp_diff_cols = [f'comp{i}_rate_percent_diff' for i in range(1, 9)]
     df['comp_diff_aggr'] = df[comp_diff_cols].fillna(0).abs().mean(axis=1)
+    df = df.drop(columns = [f'comp{i}_rate_percent_diff' for i in range(1, 9)], axis=1)
 
     #Testing new columns
-    print(df[['prop_id', 'comp_rate_sum', 'comp_inv_sum', 'comp_diff_aggr']].head())
+    #print(df[['prop_id', 'comp_rate_sum', 'comp_inv_sum', 'comp_diff_aggr']].head())
 
     
 
@@ -171,7 +181,7 @@ def preprocess(df):
     df['price_ratio_prop_history'] = df['price_usd'] / np.exp(df['prop_log_historical_price'])
 
     #aggregate into single score, not sure if this avg thing im doing tho makes sense
-    df['combined_location_score'] = (df['prop_location_score1'] + df['prop_location_score2']) / 2
+    #df['combined_location_score'] = (df['prop_location_score1'] + df['prop_location_score2']) / 2
 
     #self-explanatory
     df['price_per_adult'] = df['price_usd'] / df['srch_adults_count']
@@ -180,15 +190,16 @@ def preprocess(df):
     '''Goal: identify temporal trends'''
 
     avg_price_month = df.groupby('month')['price_usd'].agg('mean') #seasonal trends in pricing
-    avg_price_year = df.groupby('year')['price_usd'].agg('mean') #yearly trends i.e. rising prices
+    #avg_price_year = df.groupby('year')['price_usd'].agg('mean') #yearly trends i.e. rising prices
     avg_review_score_month = df.groupby('month')['prop_review_score'].agg('mean') #seasonality in customer satisfaction but might be useless (could be done for years)
-    monthly_booking_rate = df.groupby('month')['booking_bool'].agg('mean') #seasonal patterns in booking behavior (could also be done for years)
+    if train:
+        monthly_booking_rate = df.groupby('month')['booking_bool'].agg('mean') #seasonal patterns in booking behavior (could also be done for years)
+        df = df.merge(monthly_booking_rate, on='month', suffixes=('', '_booking_rate'))
 
     df = df.merge(avg_price_month, on='month', suffixes=('', '_avg_month'))
     df = df.merge(avg_review_score_month, on='month', suffixes=('', '_avg_review_month'))
-    df = df.merge(avg_price_year, on='year', suffixes=('', '_avg_year'))
-    df = df.merge(monthly_booking_rate, on='month', suffixes=('', '_booking_rate'))
-
+    #df = df.merge(avg_price_year, on='year', suffixes=('', '_avg_year'))
+    
 
     ##GROUP-AGGREGATED FEATURES##
     
@@ -199,9 +210,10 @@ def preprocess(df):
 
     #booking rate per property => identify popular properties but not sure if it fucks up training because it kinda incorporates book_bool
     #if we decide its useful we can do the same with clicks or aggregate clicks and bookings and do both
-    booking_rate_per_property = df.groupby('prop_id')['booking_bool'].mean().reset_index()
-    booking_rate_per_property.columns = ['prop_id', 'booking_rate_per_property']
-    df = df.merge(booking_rate_per_property, on='prop_id', how='left')
+    if train:
+        booking_rate_per_property = df.groupby('prop_id')['booking_bool'].mean().reset_index()
+        booking_rate_per_property.columns = ['prop_id', 'booking_rate_per_property']
+        df = df.merge(booking_rate_per_property, on='prop_id', how='left')
 
 
     location_avg_price = df.groupby('prop_country_id')['price_usd'].agg('mean')
@@ -209,36 +221,42 @@ def preprocess(df):
 
     #Feature normalization
     features_to_normalize = [
-    "prop_starrating",
+    'price_diff_visitor_history',
+    'price_ratio_prop_history',
+    "avg_review_score_per_property_dest",
+    "price_usd_avg_location",
+    "prop_starrating", 
     "prop_review_score",
     "prop_location_score1",
     "prop_location_score2",
     "prop_log_historical_price",
     "price_usd",
     "promotion_flag",
-    "orig_destination_distance",
-    "gross_bookings_usd"]
+    "orig_destination_distance"]
 
     scaler = RobustScaler()
     df[features_to_normalize] = scaler.fit_transform(df[features_to_normalize])
 
-    #TODO: Find what you will do with position (position bias)
-    #Create target variable
-    conditions = [
-    (df['click_bool'] == 1) & (df['booking_bool'] == 1),
-    (df['click_bool'] == 1) & (df['booking_bool'] == 0),
-    (df['click_bool'] == 0) & (df['booking_bool'] == 0),
-    ]
 
-    choices = [2, 1, 0]
+    if train:
+        conditions = [
+        (df['click_bool'] == 1) & (df['booking_bool'] == 1),
+        (df['click_bool'] == 1) & (df['booking_bool'] == 0),
+        (df['click_bool'] == 0) & (df['booking_bool'] == 0),
+        ]
 
-    df['suitability'] = np.select(conditions, choices, default=0)
-    df = df.drop(columns = ["click_bool", "booking_bool", "position", "gross_bookings_usd"], axis=1)
+        choices = [2, 1, 0]
 
-    y = df["suitability"]
-    X = df.drop(columns = ["suitability"], axis =1 )
+        df['suitability'] = np.select(conditions, choices, default=0)
+        df = df.drop(columns = ["click_bool", "booking_bool", "gross_bookings_usd", "position"], axis=1)
+        y = df["suitability"]
+        X = df.drop(columns = ["suitability"], axis =1 )
 
-    return X, y
+        return X,y
+    else:
+        X=df
+        return X, None
+    
 
 def position_bias_plots(df):
 
@@ -266,9 +284,111 @@ def position_bias_plots(df):
 
     plt.show()
     
-def train(df, X, y, model):
-    #TODO: stratify but on what?
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42) 
+
+def split(X, y, val_start, val_stop):
+        
+    x_new = pd.concat([X[0:val_start], X[val_stop:]])
+    y_new = np.concatenate((y[0:val_start], y[val_stop:]), axis=0)
+    x_out = X[val_start:val_stop]
+    y_out = y[val_start:val_stop]
+
+    #group query data for the Ranker 
+    groups = x_new["srch_id"].value_counts(sort=False).sort_index()
+    val_groups = x_out["srch_id"].value_counts(sort=False).sort_index()
+    #print("Inside split: \n")
+    #print(groups)
+    #print(y_new)
+
+    x_new = x_new.drop(columns = ["srch_id", "prop_id"], axis=1)
+    x_out = x_out.drop(columns = ["srch_id", "prop_id"], axis=1)
+
+
+    return (x_new, y_new, x_out, y_out, groups, val_groups) 
+
+def train(
+    x_new, y_new, x_out, y_out, boost_method, lr, groups, val_groups,
+):
+
+    ranker = lightgbm.LGBMRanker(
+        objective="lambdarank",
+        metric="ndcg",
+        n_estimators=1000,#2000,
+        learning_rate=lr,
+        label_gain=[0, 1, 2],
+        random_state=42,
+        boosting=boost_method,
+    )
+
+    gc.collect()
+    early_stopping_callback = early_stopping(stopping_rounds=150, first_metric_only=True)
+    log_evaluation_callback = log_evaluation(period=20)
+    print("The features used for training are: {}".format(x_new.columns.values))
+    ranker.fit(
+        x_new,
+        y_new,
+        eval_set=[(x_new, y_new), (x_out, y_out)],
+        eval_group=[groups, val_groups],
+        group=groups,
+        eval_at=5,
+        callbacks=[early_stopping_callback, log_evaluation_callback]
+    )
+
+    return ranker
+
+def test(X_test, output_dir):
+
+    gc.collect()
+
+    model = pickle.load(open(os.path.join(output_dir, "model.dat"), "rb"))
+
+    print("Test set has columns: {}".format(X_test.columns.values))
+    prediction = model.predict(X_test)
+
+    pred_csv_format = X_test[["srch_id", "prop_id"]]
+    pred_csv_format["predicted"] = prediction
+    pred_csv_format = pred_csv_format.sort_values(["srch_id", "predicted"], ascending=True)
+    pred_csv_format[["srch_id", "prop_id"]].to_csv(os.path.join(output_dir, "prediction.csv"), index=False)
+
+    print("Saved .csv")
+
+
+
+def run(X, y, output_dir):
+    
+    
+    boost_method = "gbdt" #'dart', 'rf' , 'goss'
+    val_n = 75000
+    lr = 0.1
+    best_score=0
+    name_of_best = ""
+    for i in range(0, int(X.shape[0] / val_n)): # num of entries / val number
+    #for i in range(0,1):
+        name = str(int(time.time())) 
+        print("Iteration of cross-validation: ", i, "\n")
+        val_start = i * val_n
+        val_end = (i + 1) * val_n
+        x_new, y_new, x_out, y_out, groups, val_groups = split( 
+            X, y, val_start, val_end
+        )
+        model = train(
+            x_new, y_new, x_out, y_out, boost_method,lr, groups, val_groups, 
+        )
+
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+        
+        gc.collect()
+        
+
+        if i == 0 or model.best_score_[f"valid_{i+1}"]['ndcg@5'] > best_score:
+            name_of_best = name
+            best_score = model.best_score_[f"valid_{i+1}"]['ndcg@5']
+            pickle.dump(model, open(os.path.join(output_dir, "model.dat"), "wb"))
+        
+
+    test_data = load_data("data/test_set_VU_DM.csv")
+    X_test, _ = preprocess(test_data, train=False)
+    test(X_test, output_dir)
 
 
 
@@ -297,10 +417,13 @@ if __name__ == "__main__":
     plt.title('prop_location_score2')
     plt.show() '''
 
+    output_dir = "output/"
+    
 
+    train_X,train_y = preprocess(df)
+    run(train_X,train_y, output_dir)
 
-    X,y = preprocess(df)
-
+    #test_X, test_y = preprocess(df, train=False)
     #plot_distr(df)
 
     #df = process_datetime(df)
